5d7019762a99f2ebf0d5d11434bd9874f154fb6c
ApolllonDev <oleksii.polovyi@gmail.com>
2020-09-05 02:27:24 -0400

    add /about

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
+      "/about": { page: "/about" },
     };
   },
 };
---
+++ pages/about.js
@@ -0,0 +1,6 @@
+const About = () => (
+  <div>
+    <p>A sample Express-Next app deployed to Heroku with Docker.</p>
+  </div>
+);
+export default About;
